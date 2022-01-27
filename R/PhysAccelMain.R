a9355542ab71679d95b6d481b078874d20215a60
Jan Brønd <jcb@video4coach.com>
2022-01-27 14:36:53 +0100

    Update PhysAccelMain.R

--- R/PhysAccelMain.R
+++ R/PhysAccelMain.R
@@ -2,14 +2,18 @@
 #This package is a wrapper of the matlab code
 pametrics <- function(filename,destinationdir,epoch,doAG,doAGI,doMAD,doENMO,doSkotte,skotteAgeGroup) {
   switch(Sys.info()[['sysname']],
-         Windows= {cmd = "";},
+         Windows= {
+           cmd = Sys.which("C:/Program Files/University of Southern Denmark/pametrics/application/pametrics.exe")
+           execmd = sprintf("%s %s %s %s %s %s %s %s %s %s",cmd,filename,destinationdir,epoch,doAG,doAGI,doMAD,doENMO,doSkotte,skotteAgeGroup);
+         },
          Linux  = {cmd = "";},
          Darwin = {
            #This is the application and the location of the libraries
            cmd = "/Applications/University_of_Southern_Denmark/pametrics/application/run_pametrics.sh /Applications/MATLAB/MATLAB_Runtime/v99/";
+           execmd = sprintf("%s %s %s %s %s %s %s %s %s %s",cmd,filename,destinationdir,epoch,doAG,doAGI,doMAD,doENMO,doSkotte,skotteAgeGroup);
+
            })
 
-  execmd = sprintf("%s %s %s %s %s %s %s %s %s",cmd,filename,destinationdir,epoch,doAG,doAGI,doMAD,doENMO,doSkotte,skotteAgeGroup);
-  #Calling the matlab application
+    #Calling the matlab application
   system(execmd,wait = TRUE);
 }
