9bd863d30f124aca87b53b8f7dcd97923b33c174
Jan Brønd <jcb@video4coach.com>
2022-03-24 09:48:05 +0100

    Update PhysAccelMain.R

--- R/PhysAccelMain.R
+++ R/PhysAccelMain.R
@@ -20,8 +20,47 @@ pametrics <- function(filename,destinationdir,epoch,doAG,doAGI,doMAD,doENMO,doSk
   system(execmd,wait = TRUE);
 }
 
+#Bulk processing of folder of cwa files
+#Uses the pametrics function
+generatePAmetricsFolder <- function(folder,destinationdir,epoch = 10,doAG = 1,doAGI = 1,doMAD = 0,doENMO = 0,doSkotte = 1,skotteAgeGroup = "adult") {
 
-countsSummary <- function(filename)
+  #Lest check the destinationdir
+  #Do we have a folder?
+  if (file.exists(destinationdir)==FALSE){
+    #The folder does not exist
+    print("The destination folder is not found. Make sure to specify an existing folder.");
+    return(0);
+  }
+
+  cwaFiles <- list.files(folder,pattern = ".cwa")
+
+
+  if (length(cwaFiles)==0)
+  {
+    print("No files found in the source folder!");
+    return(0)
+  }
+
+  myFileSep = .Platform$file.sep;
+  #Fixing the windows backslah problem
+  #The Matlab code requires backslash!!
+  if (.Platform$OS.type=="windows") {
+    #Need to fix the backslash part
+    destinationdir = gsub("/", "\\\\", destinationdir)
+    folder = gsub("/", "\\\\", folder)
+
+    myFileSep = "\\"
+  }
+
+  #invisible(lapply(cwaFiles, print))
+  for (file in cwaFiles) {
+    filename = sprintf("%s%s%s",folder,myFileSep,file)
+    print(filename)
+    pametrics(filename,destinationdir,epoch,doAG,doAGI,doMAD,doENMO,doSkotte,skotteAgeGroup)
+  }
+}
+
+countsSummary <- function(filename, id = "NA", cutPoints = c(-1,100,2000,5000,500000))
 {
 
   header = AGread::AG_meta(filename,header_timestamp_format = "%d-%m-%Y %H:%M:%S")
@@ -46,22 +85,28 @@ countsSummary <- function(filename)
     daysummary[i+1,1] = mean(daydata, na.rm = TRUE);
     daysummary[i+1,2] = sum(daydata, na.rm = TRUE);
 
-    histc = hist(daydata, breaks = c(-1,100,2000,5000,500000),plot=FALSE);
+    histc = hist(daydata, breaks = cutPoints,plot=FALSE);
     daysummary[i+1,c(6,7,8,9)] = (histc$counts * header$epoch)/60;
 
     daysummary[i+1,3] = length(daydata)
     daysummary[i+1,4] = mean(daydata[which(daydata>100)], na.rm = TRUE);
 
     daysummary[i+1,5] = sum(daydata[which(daydata>100)], na.rm = TRUE);
+
+    #daysummary[i+1,10] = id;
   }
 
   colnames(daysummary) <- c("cpm","Total","NEpochs","cpm_pa","total_pa","Sedentary","Light","Moderate","Vigorous")
+
+  daysummary = data.frame(daysummary);
+
+  daysummary$ID = rep(id, nrow(daysummary));
 
   return (daysummary);
 }
 
 
-skotteSummary <- function(filename) {
+skotteSummary <- function(filename, id = "NA") {
 
   header = AGread::AG_meta(filename,header_timestamp_format = "%d-%m-%Y %H:%M:%S")
   skotte = AGread::read_AG_counts(filename, header = TRUE,header_timestamp_format = "%d-%m-%Y %H:%M:%S")
@@ -85,5 +130,51 @@ skotteSummary <- function(filename) {
 
   colnames(skottesummary) <- c("Sitting","Move","Stand","Bike","Stairs","Run","Walk","Lying")
 
+
   return (skottesummary);
 }
+
+
+summaryIntensityFolder <- function(folder, intensityType = "AG") {
+
+  #Lest check the destinationdir
+  #Do we have a folder?
+  if (file.exists(folder)==FALSE){
+    #The folder does not exist
+    print("The folder is not found!");
+    return(0);
+  }
+
+  iType = switch(intensityType,"AG" = "_ag.csv","AGI" = "_agi.csv","MAD" = "_mad.csv", "ENMO" = "_enmo.csv")
+
+  intensityFiles <- list.files(folder,pattern = iType)
+
+  if (length(intensityFiles)==0)
+  {
+    print("No intensity files found in the source folder!");
+    return(0)
+  }
+
+  daySummary = data.frame();
+  #invisible(lapply(cwaFiles, print))
+  for (file in intensityFiles) {
+    filename = sprintf("%s%s%s",folder,.Platform$file.sep,file)
+    if (file.exists(filename)) {
+      #print(filename)
+
+      #Lets focus on the file name as ID
+      subjectID = gsub(iType,"", file)
+
+      print(subjectID)
+      if (nrow(daySummary)==0) {
+        daySummary = countsSummary(filename, id = subjectID)
+      } else {
+        daySummary = rbind(daySummary,countsSummary(filename, id = subjectID))
+      }
+    }
+
+  }
+
+  return(daySummary)
+}
+
